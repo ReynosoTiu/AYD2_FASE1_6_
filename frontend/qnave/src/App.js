7f90317ea128c8fdacd6239f62c1c2e03534d32d
Ronald Geovany Ordoñez <ronald.inge2013@gmail.com>
2024-09-28 23:31:46 -0600

    login y registro de conductor

--- frontend/qnave/src/App.js
+++ frontend/qnave/src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import HeaderConductor from "./components/header_conductor/headerConductor";
 import HomeConductor from "./views/conductor/homeConductor";
-
+import Login from "./views/Login/Login";
+import RegistrarConductor from "./views/conductor/registrarConductor";
 function LayoutConductor() {
   return <HeaderConductor></HeaderConductor>;
 }
@@ -10,8 +11,13 @@ function App() {
   return (
     <div className="container-fluid">
       <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route
+          path="/registro-conductor"
+          element={<RegistrarConductor />}
+        ></Route>
         <Route element={<LayoutConductor />}>
-          <Route path="/" element={<HomeConductor />}></Route>
+          <Route path="/homeConductor" element={<HomeConductor />}></Route>
         </Route>
       </Routes>
     </div>
